fde96291d3dbeb65d3f18102f6525249a3db154e
MB <a.terziev@live.at>
2021-03-21 00:00:50 +0300

    first commit gantry helium

---
+++ bauprojekt/cache/gantry5/g5_helium/compiled/config/5fae0ac87da073a419170e2d76896fc0.php
@@ -0,0 +1,105 @@
+<?php
+return [
+    '@class' => 'Gantry\\Component\\Config\\CompiledConfig',
+    'timestamp' => 1616273979,
+    'checksum' => '5a76bbf443c77fd15e4d745c0f01e549',
+    'files' => [
+        0 => [
+            13 => [
+                'file' => 'templates/g5_helium/custom/config/13/assignments.yaml',
+                'modified' => 1611633108
+            ],
+            14 => [
+                'file' => 'templates/g5_helium/custom/config/14/assignments.yaml',
+                'modified' => 1611633108
+            ],
+            15 => [
+                'file' => 'templates/g5_helium/custom/config/15/assignments.yaml',
+                'modified' => 1611633108
+            ],
+            16 => [
+                'file' => 'templates/g5_helium/custom/config/16/assignments.yaml',
+                'modified' => 1611633108
+            ],
+            17 => [
+                'file' => 'templates/g5_helium/custom/config/17/assignments.yaml',
+                'modified' => 1611633108
+            ],
+            18 => [
+                'file' => 'templates/g5_helium/custom/config/18/assignments.yaml',
+                'modified' => 1611633108
+            ],
+            19 => [
+                'file' => 'templates/g5_helium/custom/config/19/assignments.yaml',
+                'modified' => 1611633108
+            ],
+            20 => [
+                'file' => 'templates/g5_helium/custom/config/20/assignments.yaml',
+                'modified' => 1611633108
+            ],
+            21 => [
+                'file' => 'templates/g5_helium/custom/config/21/assignments.yaml',
+                'modified' => 1611633108
+            ],
+            27 => [
+                'file' => 'templates/g5_helium/custom/config/27/assignments.yaml',
+                'modified' => 1611633108
+            ]
+        ]
+    ],
+    'data' => [
+        13 => [
+            'menu' => [
+                
+            ]
+        ],
+        14 => [
+            'menu' => [
+                
+            ]
+        ],
+        15 => [
+            'menu' => [
+                
+            ]
+        ],
+        16 => [
+            'menu' => [
+                
+            ]
+        ],
+        17 => [
+            'menu' => [
+                
+            ]
+        ],
+        18 => [
+            'menu' => [
+                
+            ]
+        ],
+        19 => [
+            'menu' => [
+                
+            ]
+        ],
+        20 => [
+            'menu' => [
+                
+            ]
+        ],
+        21 => [
+            'menu' => [
+                
+            ]
+        ],
+        27 => [
+            'menu' => [
+                
+            ],
+            'style' => [
+                
+            ]
+        ]
+    ]
+];
---
+++ bauprojekt/cache/gantry5/g5_helium/compiled/yaml/ddbfafbaa9fba2106d0389cfe1250160.yaml.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    '@class' => 'Gantry\\Component\\File\\CompiledYamlFile',
+    'filename' => 'C:\\xampp\\htdocs\\bauprojekt\\bauprojekt\\bauprojekt/templates/g5_helium/custom/config/20/assignments.yaml',
+    'modified' => 1611633108,
+    'data' => [
+        'menu' => [
+            
+        ]
+    ]
+];
