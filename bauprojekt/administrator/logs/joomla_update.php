3ddc337cdac2ff8c3769ab3c34637449130d4992
MB <a.terziev@live.at>
2021-03-21 00:07:25 +0300

    joomla updated to 3.9.25

---
+++ bauprojekt/administrator/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-20 21:05:14 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-20T21:05:14+00:00	INFO ::1	update	Aktualisierung wurde vom Nutzer Super User (382) gestartet. Die frühere Version war 3.9.24.
+2021-03-20T21:05:18+00:00	INFO ::1	update	Aktualisierungsdatei von https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.25-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20210320%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210320T210515Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=85a2edae064bdcaae3de3b39e31a07ed481dc5d02b48ec29f1bd2ec2c58e66ba wird heruntergeladen.
+2021-03-20T21:05:27+00:00	INFO ::1	update	Datei Joomla_3.9.25-Stable-Update_Package.zip erfolgreich heruntergeladen.
+2021-03-20T21:05:27+00:00	INFO ::1	update	Neue Version wird jetzt installiert.
+2021-03-20T21:05:55+00:00	INFO ::1	update	Installation abschließen.
+2021-03-20T21:05:55+00:00	INFO ::1	update	Nicht mehr benötigte Dateien und Verzeichnisse werden gelöscht.
+2021-03-20T21:05:56+00:00	INFO ::1	update	Aufräumen nach der Installation.
+2021-03-20T21:05:56+00:00	INFO ::1	update	Aktualisierung auf Version 3.9.25 ist vollständig.
